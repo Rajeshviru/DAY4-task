657dd537c3d21955e45c692ab74e5ca88bc7a51b
rajeshviru <rajeshviru54@gmail.com>
2024-01-02 23:32:24 +0530

    first commit

---
+++ raj.js
@@ -0,0 +1,51 @@
+
+var json={
+    "personal_info": {
+        "name": "RAJESH",
+        "contact": {
+            "email": "Rajeshviru54@gmail.com",
+            "phone": "123-456-7890",
+            "address": "1/45,Naicker Street,Melappatti,Belukkurichi(P.O),Namakkal(D.T),Tamilnadu."
+        },
+        "github": "https://github.com/Rajeshviru"
+    },
+    "education": [
+        {
+            "degree": "Bachelor of Mechanical Engineering",
+            "College": "Gojan School Of Businesh And Technology",
+            "graduation_year":"2013"
+        }
+    ],
+    "experience": [
+        {
+            "title": "Production Engineer",
+            "company": "Flow Link System Private Limited",
+            "start_date": "January 2014",
+            "end_date": "Present",
+            "responsibilities": [
+                "Process schedule and execute as per planning",
+                "Stage inspection & report preparation",
+                "Preliminary inspection of parts during machining",
+                "Update inprocess inspection records"
+            ]	
+        }
+    ],
+    "skills":[
+        "Having good trouble shooting, analytical skills and technical knowledge in CNC & fanuc control, able to read all customer 2D drawings.",
+        "Good team player with leadership qualities, hardworking and enthusiasm.",
+        "Excellent communication and supervisory skills with good man handing.",
+        "Involve and develop new projects and satisfy customer by meeting high quality demands as per blue prints"
+    ]
+}
+
+console.log(json);
+
+    
+
+
+
+
+
+    
+
+    
